4429430ac631233ffe7f100539eb6c4baedd3a82
PoppaBigMac <marti>
2016-12-11 22:29:26 -0800

    Added files for the Main Table View Controller and its cells

---
+++ TodoList2/ListTitleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTitleTableViewCell.swift
+//  TodoList2
+//
+//  Created by Cesar Martinez on 12/11/16.
+//  Copyright © 2016 Cesar Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ListTitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
